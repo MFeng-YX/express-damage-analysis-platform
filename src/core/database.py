a42d2c6f6654de748d6f7e893856123661e93693
MFeng_YX <tw20200206@gmail.com>
2026-06-13 22:02:45 +0800

    写了一些配置文件和工具函数

--- src/core/database.py
+++ src/core/database.py
@@ -1,1 +1,39 @@
 import os
+import sqlalchemy as sa
+
+from contextlib import contextmanager
+from sqlalchemy import Engine
+
+_engine: Engine | None = None
+
+
+def get_engine() -> Engine:
+    """获取数据库引擎 Engine
+
+    Returns:
+        Engine : 数据库引擎
+    """
+    global _engine
+
+    if not _engine:
+        db_url = os.getenv("DB_URL")
+
+        if not db_url:
+            raise RuntimeError("环境变量 DB_URL未配置, 请配置.")
+
+        _engine = sa.create_engine(db_url, echo=True, pool_size=20)
+
+    return _engine
+
+
+@contextmanager
+def get_connection():
+    """数据库连接
+
+    Yields:
+        _type_: 自动管理数据库连接
+    """
+    engine: Engine = get_engine()
+
+    with engine.connect() as connection:
+        yield connection
